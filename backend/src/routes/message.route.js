f880cd2eb86b59074982394a5e44d5b756e8f73d
JoshitaPeluru <joshitapeluru15@gmail.com>
2025-06-22 22:26:44 +0530

    project completed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,15 +1,15 @@
 import express from "express";
-
-import {protectRoute} from "../middleware/auth.middleware.js";
-
-import { getMessages,getUsersForSidebar,sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/messages/:conversationId", getMessages); // put this before "/:id"
-router.get("/:id", protectRoute, getMessages);
+router.get("/messages/:conversationId", protectRoute, getMessages); // Good route
 router.post("/send/:id", protectRoute, sendMessage);
-
 
 export default router;
